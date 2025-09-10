456c7ada5387a96604d8617b6ce74c32a47fbf65
Agustin <correo.formal666@gmail.com>
2025-09-10 10:30:21 -0300

    Version 1.2

--- Borrador Trabajo1.py
+++ Borrador Trabajo1.py
@@ -1,26 +1,30 @@
 import matplotlib.pyplot as plt
 import sympy as sp
-#update 1.1
+#V1.2
 expresion=(input("Ingrese una funcion (X como incognita): "))
+x=int(input("Ingrese el valor de X: "))
 
 f = sp.sympify(expresion)
+x = sp.symbols('x')
 
 print(f)
+print(x)
 
-x_val=int(input("Ingrese el valor de X: "))
+x_vals = [i/10 for i in range(-10, 50)] 
+y_vals = [f.subs(x, val).evalf() for val in x_vals]
 
-x=sp.symbols('x')
-
-print(x_val)
-
-print(f.subs(x, x_val))
+print(f.subs(x, x))
 
 
 
 
-#grafica def
-#x = ([1, 2, 3, 4, 5])
-#y = ([1, 0, 2, -3, 10])
-
-#plt.plot(x, y)
-#plt.show()
+plt.figure(figsize=(10, 10))
+plt.plot(x_vals, y_vals, label=f'${sp.latex(f)}$', linewidth=2)
+plt.axhline(0, color='black', linewidth=0.5)
+plt.axvline(0, color='black', linewidth=0.5)
+plt.grid(True, alpha=0.3)
+plt.legend()
+plt.title('Gráfica Funcion')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.show()
